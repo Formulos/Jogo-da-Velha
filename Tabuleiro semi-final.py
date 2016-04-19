4dc281edc47671009377e9466603dff137d7bb02
Formulos <paulotozp@gmail.com>
2016-04-19 13:37:22 -0300

    Arumar o jogo
    so falata o jogo e arumar o label

--- Tabuleiro semi-final.py
+++ Tabuleiro semi-final.py
@@ -2,32 +2,35 @@
 """
 Created on Fri Apr 15 09:59:08 2016
 
-@author: Paulo
+@author: Paulo Tozzo
 """
 
 import tkinter as tk
 import Jogo
 
+
 class tabuleiro:
     
     def __init__(self):
-    #linhas e colunas
+        self.Jogo = Jogo.JogoVelha()
         self.window = tk.Tk()
         self.window.title("Jogo da Velha")
         self.window.geometry("300x350+350+150")
+        
+    #linhas e colunas
         for i in range (4):
             self.window.rowconfigure(i,minsize = 100,weight=1)
         for j in range(3):
             self.window.columnconfigure(j,minsize = 100,weight=1)
         self.button= []
-        self.Jogo = Jogo
         
     #label
+        self.label = tk.Label(self.window)
         self.conteudo_label = tk.StringVar()
-        label = tk.Label(self.window)
-        label.configure(textvariable=self.conteudo_label)
-        label.configure(font="Courier 20 bold")
-        label.grid(row=1, column=0,columnspan=3, padx=20, sticky="s")
+        self.label.configure(textvariable=self.conteudo_label)
+        self.label.configure(font='Courier 20 bold')
+        self.label.configure(text= 'teste')
+        self.label.grid(row=4, column=0,columnspan=3, padx=20, sticky="s")
         
     #botão
         button= self.button
@@ -43,9 +46,9 @@ class tabuleiro:
 
     def botão_apertado(self, x):
         print(x)
-#        Jogo.recebe_jogada(x)
-#        lista= self.jogo.resultados()
-        self.conteudo_label.set(self.get('1'))
+        self.Jogo.recebe_jogada(x)
+        lista= Jogo.resultados()
+        self.conteudo_label.set(self.conteudo_caixa_texto.get(lista[-1]))
         self.button[x].configure(text=lista[x])
         
     def iniciar(self):
